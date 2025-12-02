78db715e24b927a36b02b03f32f8a04e4c7387f9
Icegonzam12345 <icegonzam12@gmail.com>
2025-12-02 19:26:23 +0000

    20x20 matrix

--- snake.c
+++ snake.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <ncurses.h>
+
+#define rows 20;
+#define cols 20;
+
+char world[rows][cols];
+
+void initalworld(void){
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < cols; c++){
+             if(r == 0 || r == rows - 1 || c == 0 || c == cols - 1){
+                world[r][c] = '#';
+            } else {
+                world[r][c] = ' ';
+        }
+    }
+}
+
+
+
+
+
+
+
+
 
 int main(int argv, char **argc) {
-    
+
 }
